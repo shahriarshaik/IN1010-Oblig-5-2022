f857c74769d16301657980db754da0d378617b9a
Shahriar <meherss@uio.no>
2022-04-04 14:35:21 +0200

    la til cvs filene

--- Oblig5Del1.java
+++ Oblig5Del1.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Oblig5Del1 {
     public static void main(String[] args) {
-        
+        File fil = new File(args[0] + "/metadata.csv");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(fil);
+        } catch (FileNotFoundException e) {
+            System.out.println("Fant ikke filen!");
+        }
     }
     
 }
